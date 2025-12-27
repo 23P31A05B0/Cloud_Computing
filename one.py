7dc93fc4bf5c6f9a474245356b961a5d8ef71321
23P31A05B0 <23P31A05B0@acet.ac.in>
2025-12-27 12:08:41 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,1 +1,9 @@
-print("Hello")
+from flask import Flask
+app = Flask(__name__)
+@app.route('/hello',methods=['GET'])
+def hello():
+    return "Hello CSE"
+def hello_world():
+    return "Hello World"
+if __name__=='__main__':
+    app.run(debug=True)
